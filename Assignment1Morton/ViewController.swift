3891ee14d6ecbff49bff6d3d35f7a15be14d380b
Sam Morton <sammorton44@gmail.com>
2023-02-15 00:41:54 -0500

    finished assignment 1:
    '

---
+++ Assignment1Morton/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Assignment1Morton
+//
+//  Created by Samuel H Morton on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var ProfilePic: UIImageView!
+    @IBOutlet weak var Name: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        styleProfilePic()
+    }
+    
+    private func styleProfilePic() {
+        ProfilePic.layer.cornerRadius = min(
+            ProfilePic.frame.width,
+            ProfilePic.frame.height
+        ) / 2.0
+    }
+}
+
